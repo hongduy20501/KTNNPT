2525906ea8b3edbc042273e3517045ca1de23cd7
Nobi <nobi@nobidev.com>
2023-03-01 14:33:27 +0700

    Add UI

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const models = require('./models');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/products', (req, res) => {
     res.json(models.products.listProducts());
